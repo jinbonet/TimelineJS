42302f0dc4dd9470892f6522dd24b0d6b64bc2d2
Kyuman Hwang <hwangkm@member.jinbo.net>
2014-04-09 17:40:48 +0900

    fetch easing animate for IE8

--- model/touchcarousel/js/jquery.taogi.touchcarousel.js
+++ model/touchcarousel/js/jquery.taogi.touchcarousel.js
@@ -5837,119 +5837,6 @@ function onYouTubePlayerAPIReady() {
  *
  */
 
-	$.Easingfunc = function(easing,t,b,c,d) {
-		switch(easing) {
-			case 'ease-in':
-				return c*Math.pow(t/d,4.5)+ b;
-			case 'ease-out':
-				t/=d;
-				return c*t*t + b;
-			case 'ease-in-out':
-				if ((t/=d/2) < 1) return c/2*t*t + b;
-				return -c/2 * ((--t)*(t-2) - 1) + b;
-			case 'linearTween':
-				return c*t/d + b;
-			// quadratic easing in - accelerating from zero velocity
-			case 'easeInQuad':
-				t /= d;
-				return c*t*t + b;
-			// quadratic easing out - decelerating to zero velocity
-			case 'easeOutQuad':
-				t /= d;
-				return -c * t*(t-2) + b;
-			// quadratic easing in/out - acceleration until halfway, then deceleration
-			case 'easeInOutQuad':
-				t /= d/2;
-				if (t < 1) return c/2*t*t + b;
-				t--;
-				return -c/2 * (t*(t-2) - 1) + b;
-			// cubic easing in - accelerating from zero velocity
-			case 'easeInCubic':
-				t /= d;
-				return c*t*t*t + b;
-			// cubic easing out - decelerating to zero velocity
-			case 'easeOutCubic':
-				t /= d;
-				t--;
-				return c*(t*t*t + 1) + b;
-			// cubic easing in/out - acceleration until halfway, then deceleration
-			case 'easeInOutCubic':
-				t /= d/2;
-				if (t < 1) return c/2*t*t*t + b;
-				t -= 2;
-				return c/2*(t*t*t + 2) + b;
-			// quartic easing in - accelerating from zero velocity
-			case 'easeInQuart':
-				t /= d;
-				return c*t*t*t*t + b;
-			// quartic easing out - decelerating to zero velocity
-			case 'easeOutQuart':
-				t /= d;
-				t--;
-				return -c * (t*t*t*t - 1) + b;
-			// quartic easing in/out - acceleration until halfway, then deceleration
-			case 'easeInOutQuart':
-				t /= d/2;
-				if (t < 1) return c/2*t*t*t*t + b;
-				t -= 2;
-				return -c/2 * (t*t*t*t - 2) + b;
-			// quintic easing in - accelerating from zero velocity
-			case 'easeInQuint':
-				t /= d;
-				return c*t*t*t*t*t + b;
-			// quintic easing out - decelerating to zero velocity
-			case 'easeOutQuint':
-				t /= d;
-				t--;
-				return c*(t*t*t*t*t + 1) + b;
-			// quintic easing in/out - acceleration until halfway, then deceleration
-			case 'easeInOutQuint':
-				t /= d/2;
-				if (t < 1) return c/2*t*t*t*t*t + b;
-				t -= 2;
-				return c/2*(t*t*t*t*t + 2) + b;
-			// sinusoidal easing in - accelerating from zero velocity
-			case 'easeInSine':
-				return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
-			// sinusoidal easing out - decelerating to zero velocity
-			case 'easeOutSine':
-				return c * Math.sin(t/d * (Math.PI/2)) + b;
-			// sinusoidal easing in/out - accelerating until halfway, then decelerating
-			case 'easeInOutSine':
-				return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
-			// exponential easing in - accelerating from zero velocity
-			case 'easeInExpo':
-				return c * Math.pow( 2, 10 * (t/d - 1) ) + b;
-			// exponential easing out - decelerating to zero velocity
-			case 'easeOutExpo':
-				return c * ( -Math.pow( 2, -10 * t/d ) + 1 ) + b;
-			// exponential easing in/out - accelerating until halfway, then decelerating
-			case 'easeInOutExpo':
-				t /= d/2;
-				if (t < 1) return c/2 * Math.pow( 2, 10 * (t - 1) ) + b;
-				t--;
-				return c/2 * ( -Math.pow( 2, -10 * t) + 2 ) + b;
-			// circular easing in - accelerating from zero velocity
-			case 'easeInCirc':
-				t /= d;
-				return -c * (Math.sqrt(1 - t*t) - 1) + b;
-			// circular easing out - decelerating to zero velocity
-			case 'easeOutCirc':
-				t /= d;
-				t--;
-				return c * Math.sqrt(1 - t*t) + b;
-			// circular easing in/out - acceleration until halfway, then deceleration
-			case 'easeInOutCirc':
-				t /= d/2;
-				if (t < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
-				t -= 2;
-				return c/2 * (Math.sqrt(1 - t*t) + 1) + b;
-			default:
-				return c*t/d + b;
-		}
-		return null;
-	}
-
 	function TaogiTouchCarousel(element,options) {
 		var self = this;
 		TaogiTouchCarouselObj = this;
@@ -6581,7 +6468,8 @@ function onYouTubePlayerAPIReady() {
 					var lastendPos = this.lastendPos;
 					var maxPos = this._totalItemWidth-this.tlcWidth;
 
-					var easing = (this.hasTouch || this._useWebkitTransition) ? 'easeOutCubic' : $.cssEase['easeOutCubic'];
+					var easing = 'easeOutCubic';
+					var bounceeasing = 'easeOutCubic';
 
 					if(lastendPos + S > 0) {
 						if(lastendPos > 0) {
@@ -6594,7 +6482,7 @@ function onYouTubePlayerAPIReady() {
 								true,
 								0,
 								400,
-								'easeOutCubic');
+								bounceeasing);
 						}
 					} else if(lastendPos + S < maxPos*(-1)) {
 						if(lastendPos < maxPos*(-1)) {
@@ -6607,7 +6495,7 @@ function onYouTubePlayerAPIReady() {
 								true,
 								maxPos*(-1),
 								400,
-								'easeOutCubic');
+								bounceeasing);
 						}
 					} else {
 						this.animateTo(lastendPos + S, t, easing);
